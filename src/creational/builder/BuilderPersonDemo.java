04d449222422d68cec967ec57ebe4b4174279183
Eduard Dones <eduard.dones@gmail.com>
2018-08-22 13:17:41 +0800

    Updated readme

--- src/creational/builder/BuilderPersonDemo.java
+++ src/creational/builder/BuilderPersonDemo.java
@@ -3,8 +3,11 @@ package creational.builder;
 public class BuilderPersonDemo {
     public static void main(String args[]) {
         PersonBuilder builder = new PersonBuilder();
-        builder.firstName("John").lastName("Wick").age(30).gender("Male");
-        Person person = builder.build();
-        System.out.println(person.toString());
+        Person john = builder.firstName("John").lastName("Wick").age(30).gender("Male").build();
+        System.out.println(john.toString());
+
+        builder = new PersonBuilder();
+        Person jason = builder.firstName("Jason").lastName("Bourne").age(40).build();
+        System.out.println(jason.toString());
     }
 }
