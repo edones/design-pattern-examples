661fcb6a7e54f877d681f7c97940a92a1502fd39
Eduard Dones <eduard.dones@gmail.com>
2018-08-24 16:03:47 +0800

    Add initial code

---
+++ src/behavioral/visitor/VisitorDemo.java
@@ -0,0 +1,12 @@
+package behavioral.visitor;
+
+public class VisitorDemo {
+    public static void main(String[] args) {
+        //Needs: Visitor, Element, ConcreteVisitor, ConcreteElement
+        //1. create element interface & concrete element
+        //2. add the 'accept(Visitor visitor)' method in element interface
+        //     with hook in body:   visitor.visit(this);
+        //3. create visitor interface
+        //4. add concrete visitor
+    }
+}
